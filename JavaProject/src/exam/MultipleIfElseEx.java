5345cff2fc0d06f4400bd1ba1f21616e72a097ed
Park Eun Jin <eunjin950317@gmail.com>
2026-07-22 08:54:14 +0900

    MultipleIfElseEx

--- JavaProject/src/exam/MultipleIfElseEx.java
+++ JavaProject/src/exam/MultipleIfElseEx.java
@@ -14,17 +14,12 @@ public class MultipleIfElseEx {
 		System.out.print("숫자3 입력 : ");
 		num3 = sc.nextInt();
 		
-		if(num1 > num2) {
-			if(num1 > num3) 
-				System.out.println("가장 큰 수 : " + num1);
-		}
-		if(num2 > num1) {
-			if(num2 > num3)
-				System.out.println("가장 큰 수 : " + num2);
-		}
-		if(num3 > num2) {
-			if(num3 > num1)
-				System.out.println("가장 큰 수 : " + num3);
+		if(num1 >= num2 && num1 >= num3) {
+		    System.out.println("가장 큰 수 : " + num1);
+		} else if(num2 >= num1 && num2 >= num3) {
+		    System.out.println("가장 큰 수 : " + num2);
+		} else {
+		    System.out.println("가장 큰 수 : " + num3);
 		}
 
 	}
