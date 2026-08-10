f796d45d6715844c7b97e0930f9c4d23ef7c96f9
Park Eun Jin <eunjin950317@gmail.com>
2026-08-10 16:49:14 +0900

    0810 JDBC수업

---
+++ JavaProject/src/jdbc/DBTestConn.java
@@ -0,0 +1,37 @@
+package jdbc;
+
+//1. 필요패키지 import, db 연결 관련 모든 기능 java.sql패키지에 있음
+import java.sql.Connection; //db 연결관련 인터페이스
+import java.sql.DriverManager; //Connection 구현객체 반환해줌
+
+public class DBTestConn {
+
+	public static void main(String[] args) {
+		//db연결 test
+		Connection con = null;
+		
+		// 외부 자원 활용 : try ~ catch 구문 안에서 진행되거나 throws문을 추가하거나
+		try {
+			//dbms 연결 관련 코드
+			// JDBC DIRVER클래스의 객체 생성 : 런타임시 JDBC 드라이버 로드(자동로드 : 생략가능)
+			//Class.forName("oracle.jdbc.dirver.OracleDriver");
+			//접속 변수 생성
+			//"oracle 접속종류: @dbms주소(도메인, ip):포트:sid 또는 포트/ 서비스name
+			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			String user = "system";
+			String pwd = "1234";
+			
+			//2. Connection interface 참조변수에 구현객체 대입(db 접속 후 접속 객체 반환) -> DriverManager
+			con = DriverManager.getConnection(url, user, pwd);
+			// db 접속 실패하면 catch, 저장값은 null
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		if(con != null) {
+			System.out.println("DB 연결 성공");
+		}else {
+			System.out.println("DB 연결 실패");
+		}
+		
+	}
+}
