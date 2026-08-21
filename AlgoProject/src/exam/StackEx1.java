fbf859c5f8955c52b833bd8ae00e0938e3aa803c
Park Eun Jin <eunjin950317@gmail.com>
2026-08-21 17:46:50 +0900

    0821

--- AlgoProject/src/exam/StackEx1.java
+++ AlgoProject/src/exam/StackEx1.java
@@ -6,6 +6,18 @@ import datastructure.stack.Stack;
 public class StackEx1 {
 	
 	public static void main(String[] args) {
+		/*
+		 * 수식의 괄호 매칭 점검 프로그램
+		 * 모든 괄호는 왼쪽 괄호가 나오고 오른쪽 괄호가 나옴
+		 * 괄호는 가장 늦게 열린 괄호가 가장 먼저 닫힘
+		 * ({[]})
+		 * 점검1 : stack이 empty인데 }]) 만나서 pop을 호출 - 왼쪽 괄호 없음
+		 * 점검2 : 수식의 괄호 검증이 끝났는데 스택이 비어있지 않은 경우 - 오른쪽 괄호가 없는 경우
+		 * 점검3 : 스택에서 pop한 결과가 현재 보고 있는 오른쪽 괄호의 왼쪽 괄호가 아닐 경우 - 괄호 짝이 안맞는 경우
+		 * 
+		 */
+		
+		
 		// 괄호가 포함된 수식을 입력받아 해당 수식의 괄호가 올바른지 확인하는 문제
 		Scanner sc = new Scanner(System.in);
 		System.out.print("괄호가 포함된 수식을 입력하세요");
---
+++ AlgoProject/src/exam/StackEx1Answer.java
@@ -0,0 +1,60 @@
+package exam;
+
+import java.util.Scanner;
+
+import datastructure.stack.Stack;
+
+public class StackEx1Answer {
+
+	public static void main(String[] args) {
+		/*
+		 * 수식의 괄호 매칭 점검 프로그램
+		 * 모든 괄호는 왼쪽 괄호가 나오고 오른쪽 괄호가 나옴
+		 * 괄호는 가장 늦게 열린 괄호가 가장 먼저 닫힘
+		 * ({[]})
+		 * 점검1 : stack이 empty인데 }]) 만나서 pop을 호출 - 왼쪽 괄호 없음
+		 * 점검2 : 수식의 괄호 검증이 끝났는데 스택이 비어있지 않은 경우 - 오른쪽 괄호가 없는 경우
+		 * 점검3 : 스택에서 pop한 결과가 현재 보고 있는 오른쪽 괄호의 왼쪽 괄호가 아닐 경우 - 괄호 짝이 안맞는 경우
+		 * 
+		 */
+		Scanner sc = new Scanner(System.in);
+		Stack stack = new Stack(100);
+		
+		String correct = sc.nextLine();
+		int answer = 0;
+		
+		char[] arr = correct.toCharArray();// 문자열을 한문자씩 저장된 char배열로 반환
+		
+		for(char chr : arr) {
+			stackPush(stack, chr);
+			answer = check(stack, chr);
+			if(answer !=0 )break; //
+		}
+		if(stack.isEmpty() && answer == 0)
+			System.out.println("수식의 괄호가 모두 정상입니다");
+		else
+			System.out.println("수식 괄호에 오류가 있습니다");
+
+	}
+	private static void stackPush(Stack stack, char c) {
+		if((c=='(')||(c=='{')||(c=='[')) {
+			stack.push(c);
+		}
+	}
+	
+	private static int check(Stack stack, char c) {
+		if((c==')')||(c=='}')||(c==']')) {
+			if(stack.isEmpty()) return 1; //검증1
+			else {
+				char p = stack.pop();
+				if((p == '('&&c==')')||(p == '{'&&c=='}')||(p == '['&&c==']')) {//검증3
+					return 0;
+				}
+				else return 3;
+			}
+			
+		}
+		return 0;
+	}
+
+}
