dc1c18735da355bc0be4818223a7b9ac8829493a
Park Eun Jin <eunjin950317@gmail.com>
2026-08-31 00:22:37 +0900

    0830

--- Servlet01/src/main/java/exam/ojdbc/DBConnect.java
+++ Servlet01/src/main/java/exam/ojdbc/DBConnect.java
@@ -6,19 +6,28 @@ import java.sql.DriverManager;
 public class DBConnect {
 	
 	public Connection getConnection() {
-	Connection con = null;
-	
-	try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = null;
 		
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String user = "PDBSQL_SELECT";
-		String pwd = "1234";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			String user = "SQL_SELECT";
+			String pwd = "1234";
+			
+			con = DriverManager.getConnection(url, user, pwd);
+			
+		} catch (ClassNotFoundException e) {
+			// ojdbc 드라이버(jar)가 프로젝트에 없을 때 발생
+			e.printStackTrace();
+			throw new RuntimeException("Oracle JDBC 드라이버를 찾을 수 없습니다. WEB-INF/lib에 ojdbc jar가 있는지 확인하세요.", e);
+			
+		} catch (Exception e) {
+			// URL/계정/비밀번호/리스너 문제 등 접속 관련 예외
+			e.printStackTrace();
+			throw new RuntimeException("DB 연결에 실패했습니다: " + e.getMessage(), e);
+		}
 		
-		con = DriverManager.getConnection(url, user, pwd);
-	}catch(Exception e) {
-		e.printStackTrace();
-	}
-	return con;
+		return con;
 	}
 }
